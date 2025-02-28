6ff56f4a4665f75d0ab32ae87877d952e9e5f79b
santalov <santalov.stas@gmail.com>
2025-02-28 03:40:09 +0300

    add coins script

---
+++ fullsail_config/scripts/coin_tkna.sh
@@ -0,0 +1,9 @@
+export COIN_TYPE=0x1018b0843a724fd966b37f018bbc489918b6594144451cf4b481d392c9a0a463::token_a::TOKEN_A
+export COIN_NAME='"Token_A"'
+export COIN_SYMBOL='"TKNA"'
+export COIN_COINGECKO_ID='""'
+export COIN_PYTH_ID='""'
+
+export COIN_DECIMALS=9
+export COIN_LOGO_URL='"https://assets.coingecko.com/coins/images/54467/standard/RING.png?1739897455"'
+export COIN_PROJECT_URL='""'
