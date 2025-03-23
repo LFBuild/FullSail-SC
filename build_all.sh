9c26cb79eaad08a92e82c351b68f97b15c843ecf
santalov <santalov.stas@gmail.com>
2025-03-23 14:31:58 +0300

    testnet publications of the packages

---
+++ build_all.sh
@@ -0,0 +1,8 @@
+# call this script to update all git dependencies of all packages and build them
+cd ./move_stl && sui move build && cd ../
+cd ./integer_mate && sui move build && cd ../
+cd ./gauge_cap && sui move build && cd ../
+cd ./clmm_pool && sui move build && cd ../
+cd ./distribution && sui move build && cd ../
+cd ./integrate && sui move build && cd ../
+cd ./fullsail_config && sui move build && cd ../
