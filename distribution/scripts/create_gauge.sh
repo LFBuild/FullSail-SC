c4c4e6e75582e8c657cc788afa473388ff51eba1
santalov <santalov.stas@gmail.com>
2025-04-10 14:21:10 +0300

    finalize depoyment, setup distribution and guage

--- distribution/scripts/create_gauge.sh
+++ distribution/scripts/create_gauge.sh
@@ -1,10 +1,10 @@
 source ./export.sh
 
-export COIN_A=0xd80ee37bf2520ef907756ba80327c6f546bf1bdbe9d1e3c149f961591d0e5ef9::sail_token::SAIL_TOKEN
-export COIN_B=0x1018b0843a724fd966b37f018bbc489918b6594144451cf4b481d392c9a0a463::token_a::TOKEN_A
-export POOL=0x5cc46ab150b14f2d2239b6efca80cd887ebd89776b9ab325e04eaf2513bbba52
+export COIN_A=0xda1f9eaf3d10cd6fa609d3061ac48d640c0aeb36fb031125a263736a0ae0be29::token_b::TOKEN_B
+export COIN_B=0x4e57cf0fd73647d44d2191fbb3028176893c07388d9472df07124fe7f72c0a66::sail_token::SAIL_TOKEN
+export POOL=0x163b704cccf75820c62b6b0d3a3f1b67e15b5c9c9a44d795a13b76b2bec609f3
 
 sui client ptb \
 --move-call $PACKAGE::voter::create_gauge "<$COIN_A,$COIN_B,$FULLSAIL_TOKEN_TYPE>" @$VOTER @$DISTRIBUTION_CONFIG @$CREATE_CAP @$GOVERNOR_CAP @$VOTING_ESCROW @$POOL @$CLOCK \
 --assign gauge \
---move-call sui::transfer::public_share_object "<$ORIGINAL_PACKAGE::gauge::Gauge<$COIN_A,$COIN_B,$FULLSAIL_TOKEN_TYPE>>" gauge --dry-run
+--move-call sui::transfer::public_share_object "<$ORIGINAL_PACKAGE::gauge::Gauge<$COIN_A,$COIN_B,$FULLSAIL_TOKEN_TYPE>>" gauge
