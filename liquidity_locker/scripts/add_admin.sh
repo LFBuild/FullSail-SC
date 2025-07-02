405133be58eb226a93a5d065405d5fdfe2060b6f
santalov <santalov.stas@gmail.com>
2025-07-02 15:37:15 +0500

    setup distribution

--- liquidity_locker/scripts/add_admin.sh
+++ liquidity_locker/scripts/add_admin.sh
@@ -1,6 +1,7 @@
 source ./export.sh
 
-export NEW_ADMIN=0x8c30bf5bfd2fb00bd9198599ea1bf6ae84f3b1855a238ed458765dd2adce0340
+export NEW_ADMIN=0x7f2bc2cadcead6dc4706c3259ff0e55fcfd6afd5b9134a0978b0ad22453ac486
 
 sui client ptb \
---move-call $PACKAGE::liquidity_lock_v1::add_admin @$SUPER_ADMIN_CAP @$LOCKER_V1 @$NEW_ADMIN
+--move-call $PACKAGE::liquidity_lock_v1::add_admin @$SUPER_ADMIN_CAP_LOCK @$LOCKER_V1 @$NEW_ADMIN \
+--move-call $PACKAGE::pool_tranche::add_admin @$SUPER_ADMIN_CAP_TRANCH @$POOL_TRANCH_MANAGER @$NEW_ADMIN 
