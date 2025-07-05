60060aee8f5f3f8fe8c890b381cf2170eb669437
santalov <santalov.stas@gmail.com>
2025-07-05 19:20:25 +0500

    mint tokens

--- distribution/scripts/grant_dist_governor.sh
+++ distribution/scripts/grant_dist_governor.sh
@@ -1,6 +1,6 @@
 source ./export.sh
 
-export WHO=$ADDR
+export WHO=0x1e4662015b374c68f205a90bddc7bc930a373c30aa43bffab76933b2039cc273
 
 sui client ptb \
 --move-call $PACKAGE::minter::grant_distribute_governor @$MINTER_PUBLISHER @$WHO
