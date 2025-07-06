da41c64d6a54884c3b0f8faeb3aa67aa2c2dd19a
santalov <santalov.stas@gmail.com>
2025-07-06 21:13:55 +0500

    inject fees

--- distribution/scripts/grant_dist_governor.sh
+++ distribution/scripts/grant_dist_governor.sh
@@ -1,6 +1,6 @@
 source ./export.sh
 
-export WHO=0x1e4662015b374c68f205a90bddc7bc930a373c30aa43bffab76933b2039cc273
+export WHO=0x8c30bf5bfd2fb00bd9198599ea1bf6ae84f3b1855a238ed458765dd2adce0340
 
 sui client ptb \
 --move-call $PACKAGE::minter::grant_distribute_governor @$MINTER_PUBLISHER @$WHO
