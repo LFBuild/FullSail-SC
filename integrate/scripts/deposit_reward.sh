bfcb9e85f4431e4eb64f9ccbe1be4b7761bd0bce
santalov <santalov.stas@gmail.com>
2025-07-19 12:00:13 +0500

    start new distribution

--- integrate/scripts/deposit_reward.sh
+++ integrate/scripts/deposit_reward.sh
@@ -1,7 +1,7 @@
 source ./export.sh
 
 export TOTAL_REWARD_AMOUNT=2500000000000 # set the total reward amount
-export COIN_ID=0x0000000000000000000000000000000000000000000000000000000000000000
+export COIN_ID=0xb8284f585c41a9fe029b3a6eb97c95ad3bf8ff108110f0f8b4d0f2f750466b3c
 
 sui client call \
   --package $PACKAGE \
