140d8df89ba525de10d7e1aa91197b3e8e5c0993
santalov <santalov.stas@gmail.com>
2025-07-29 05:58:45 +0500

    scripts for IKA pool

---
+++ integrate/scripts/ika_deposit_reward_pre_sail.sh
@@ -0,0 +1,18 @@
+export TOTAL_REWARD_AMOUNT=1000000000000 # 1m preSAIL
+export COIN_ID=0xafb67522c6716aa0c35734d255cc7ba365a0d4c26f2cb0f3ab6dbe9a9694a5de?
+
+export REWARD_TOKEN_TYPE=0x0::pre_sail::PRE_SAIL?
+export GLOBAL_CONFIG=0xe93baa80cb570b3a494cbf0621b2ba96bc993926d34dc92508c9446f9a05d615
+export REWARDER_GLOBAL_VAULT=0xfb971d3a2fb98bde74e1c30ba15a3d8bef60a02789e59ae0b91660aeed3e64e1
+export PACKAGE=0xe1b7d5fd116fea5a8f8e85c13754248d56626a8d0a614b7d916c2348d8323149
+
+sui client call \
+  --package $PACKAGE \
+  --module rewarder_script \
+  --function deposit_reward \
+  --type-args $REWARD_TOKEN_TYPE \
+  --args \
+    $GLOBAL_CONFIG \
+    $REWARDER_GLOBAL_VAULT \
+    "[$COIN_ID]" \
+    $TOTAL_REWARD_AMOUNT
