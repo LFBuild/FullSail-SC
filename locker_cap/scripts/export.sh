f07b18adaef6ceaf9455914026df602812e71305
Alexey <komalex203@gmail.com>
2025-08-01 13:38:57 +0300

    added scripts

---
+++ locker_cap/scripts/export.sh
@@ -0,0 +1,7 @@
+export ADDR=$(sui client active-address)
+
+# Package ID (нужно будет обновить после публикации)
+export PACKAGE="0xf662ffe4b2eb869639762d910a110ee62cb10502627b5f22900e98dd383eb13f"
+
+# CreateCap ID
+export CREATE_CAP="0x3be3245d0ced9d3c30c5af4c5bba923833c9adde2ec3eca221f2e35cc42f3aed" 
