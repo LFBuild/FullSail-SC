9e5325c0e3e06237e3dbcbaa1399cf3e5065b883
santalov <santalov.stas@gmail.com>
2025-08-05 22:33:22 +0500

    update lock periods

--- liquidity_locker/scripts/update_lock_periods.sh
+++ liquidity_locker/scripts/update_lock_periods.sh
@@ -5,8 +5,8 @@ source ./export.sh
 # periods_post_lockdown: [10, 20, 30] - периоды после блокировки в эпохах
 
 sui client ptb \
---make-move-vec "<u64>" "[1, 13, 26, 39, 52]" \
+--make-move-vec "<u64>" "[4, 13, 26, 39, 52]" \
 --assign periods_blocking \
---make-move-vec "<u64>" "[1, 2, 3, 4, 4]" \
+--make-move-vec "<u64>" "[0, 0, 0, 0, 0]" \
 --assign periods_post_lockdown \
 --move-call $PACKAGE::liquidity_lock_v1::update_lock_periods @$LOCKER_V1 periods_blocking periods_post_lockdown 
