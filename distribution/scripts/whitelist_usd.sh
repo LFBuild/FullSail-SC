f4f8a6d246ca81ab67538174df878057f7a7c9ea
santalov <santalov.stas@gmail.com>
2025-08-29 03:17:31 +0500

    add aggregator and whitelist usd

--- distribution/scripts/whitelist_usd.sh
+++ distribution/scripts/whitelist_usd.sh
@@ -4,6 +4,6 @@ export USD_TYPE=0x1018b0843a724fd966b37f018bbc489918b6594144451cf4b481d392c9a0a4
 export METADATA=0x57ec0b6c22c3dc2c5cbbabb8a8e8baa647726128df41f816c62f22af6bc785bb
 
 sui client ptb \
---move-call $PACKAGE::minter::whitelist_usd "<$FULLSAIL_TOKEN_TYPE,$USD_TYPE>" @$MINTER @$MINTER_ADMIN_CAP true 
+--move-call $PACKAGE::minter::whitelist_usd "<$FULLSAIL_TOKEN_TYPE,$USD_TYPE>" @$MINTER @$DISTRIBUTION_CONFIG @$MINTER_ADMIN_CAP true 
 
 
--- price_monitor/scripts/add_aggregator.sh
+++ price_monitor/scripts/add_aggregator.sh
@@ -1,9 +1,9 @@
 source ./export.sh
 
 export AGGREGATOR=0xffc454dfd3e8543d8bb097a3bf8acfb9d721eed5820d86aa31a331291ff5af5f
-export POOL=0xecd2248bff3ebe159e236076324c70f15df24c50ec81a8213bc91b1db6664fc1
+export POOL=0xcd24a573e09c3aaf67850ee3c6511e4508d8faf897eb12ab38b580c894b5d976
 
 sui client ptb \
 --make-move-vec '<sui::object::ID>' "[@$POOL]" \
 --assign pool_ids \
---move-call $PACKAGE::price_monitor::add_aggregator @$MONITOR @$AGGREGATOR pool_ids
+--move-call $PACKAGE::price_monitor::add_aggregator @$MONITOR @$AGGREGATOR pool_ids "vector[9]" "vector[6]"
