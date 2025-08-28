51a953cae5c56e81b80164794e8ada34b8b60e30
santalov <santalov.stas@gmail.com>
2025-08-29 03:06:53 +0500

    new airdrop

--- airdrop/scripts/new_exchange_airdrop.sh
+++ airdrop/scripts/new_exchange_airdrop.sh
@@ -1,6 +1,6 @@
 source ./export.sh
 
-sui client ptb --split-coins @0xd2c445312d1c60c95184cef3c2d68092d6bb363d9b89a5d76fff8bc8d0680b81 "[1000000000000]" \
+sui client ptb --split-coins @0x7d4605c9ae06459e270ebf98a3c3cdd6c2e354f1287505a26b9549ea28cce9c9 "[100000000000]" \
   --assign new_coins \
   --move-call $PACKAGE::exchange_airdrop::new "<$COIN_IN, $SAIL_TOKEN_TYPE>" new_coins.0 0 --assign airdrop \
   --move-call  sui::transfer::public_share_object "<$PACKAGE::exchange_airdrop::ExchangeAirdrop<$COIN_IN, $SAIL_TOKEN_TYPE>>" airdrop.0 \
