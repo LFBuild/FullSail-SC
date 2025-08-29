ee74dde2f6a31b24cc259ed40b2ac077e784b136
santalov <santalov.stas@gmail.com>
2025-08-30 03:50:27 +0500

    script to create new ve airdrop

--- airdrop/scripts/new_ve_airdrop.sh
+++ airdrop/scripts/new_ve_airdrop.sh
@@ -1,8 +1,12 @@
 source ./export.sh
 
-sui client ptb --split-coins @0xe1987f11b3b24cfdd0890f0446002468f18dc9b7bf7b1c4af9e3038155653b1f "[100000000000]" \
+# !!! don't forget to put the time somewhere in the future
+
+sui client ptb \
+  --sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
+  --split-coins @0xef1835311f0d615bf11b2aa2068231e724a8809080ad923393adfe867f929cf0 "[8800001000000]" \
   --assign new_coins \
   --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[242,  58,  53,  86,  37, 215, 205, 194,  24, 119, 112, 171, 103,   2, 231, 248,15, 212, 146, 206, 182, 224,  77, 241,82,  30, 190, 178, 168,   2,  67, 236] 1756342500000 @$CLOCK \
   --assign airdrop \
-  --transfer-objects "[airdrop.1]" @0xc3c7b01f09bfb204f93de85afa0a271e5bfabac31e566f7e997b9a8685f18967
+  --transfer-objects "[airdrop.1]" @0xc3c7b01f09bfb204f93de85afa0a271e5bfabac31e566f7e997b9a8685f18967 \
   --move-call  sui::transfer::public_share_object "<$PACKAGE::ve_airdrop::VeAirdrop<$SAIL_TOKEN_TYPE>>" airdrop.0
