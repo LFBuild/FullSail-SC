65e3619e7456fa65ae6c6f75e2ae5c1f2e23dee1
santalov <santalov.stas@gmail.com>
2025-09-04 04:32:34 +0500

    scripts

--- governance/scripts/create_gauge.sh
+++ governance/scripts/create_gauge.sh
@@ -1,7 +1,4 @@
 source ./export.sh
-source ./pools/pool_sail_tkna.sh
-
-export GAUGE_BASE_EMISSIONS=10000000000
 
 sui client ptb \
 --move-call $PACKAGE::minter::create_gauge "<$COIN_A,$COIN_B,$FULLSAIL_TOKEN_TYPE>" @$MINTER @$VOTER @$DISTRIBUTION_CONFIG @$CREATE_CAP @$MINTER_ADMIN_CAP @$VOTING_ESCROW @$POOL $GAUGE_BASE_EMISSIONS @$CLOCK \
