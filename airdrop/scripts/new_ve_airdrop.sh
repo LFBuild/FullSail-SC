41be805c97fbf91d9a1d13236f6ce5c1e288f2b7
santalov <santalov.stas@gmail.com>
2025-09-04 00:42:19 +0500

    restore prod artifacts

--- airdrop/scripts/new_ve_airdrop.sh
+++ airdrop/scripts/new_ve_airdrop.sh
@@ -2,9 +2,11 @@ source ./export.sh
 
 # !!! don't forget to put the time somewhere in the future
 
-sui client ptb --split-coins @0xf8cf04ceb3a40eff1b72776c7d75e24f8bd457291845c60aef32c0490d6ca971 "[100000000000]" \
+sui client ptb \
+  --sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
+  --split-coins @0xef1835311f0d615bf11b2aa2068231e724a8809080ad923393adfe867f929cf0 "[8800001000000]" \
   --assign new_coins \
-  --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[37,86,32,161,11,231,36,245,142,60,82,84,208,83,49,63,221,166,142,231,45,150,4,70,43,236,11,67,33,84,127,97] 1756857566606 @$CLOCK \
+  --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[253,174,168,137,41,72,76,116,253,189,195,181,171,145,21,103,221,153,39,0,43,132,225,232,24,225,140,15,61,169,222,161] 1756562785837 @$CLOCK \
   --assign airdrop \
-  --move-call  sui::transfer::public_share_object "<$PACKAGE::ve_airdrop::VeAirdrop<$SAIL_TOKEN_TYPE>>" airdrop.0 \
-  --transfer-objects "[airdrop.1]" @$ADDR
+  --transfer-objects "[airdrop.1]" @0xc3c7b01f09bfb204f93de85afa0a271e5bfabac31e566f7e997b9a8685f18967 \
+  --move-call  sui::transfer::public_share_object "<$PACKAGE::ve_airdrop::VeAirdrop<$SAIL_TOKEN_TYPE>>" airdrop.0
