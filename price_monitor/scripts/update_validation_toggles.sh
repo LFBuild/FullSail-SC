69c18e97cca10e86935e40f18b674891af61d977
santalov <santalov.stas@gmail.com>
2025-09-04 15:02:24 +0500

    update time configs

--- price_monitor/scripts/update_validation_toggles.sh
+++ price_monitor/scripts/update_validation_toggles.sh
@@ -1,4 +1,4 @@
 source ./export.sh
 
 sui client ptb \
---move-call $PACKAGE::price_monitor::update_validation_toggles @$MONITOR true true true
+--move-call $PACKAGE::price_monitor::update_validation_toggles @$MONITOR false false false
