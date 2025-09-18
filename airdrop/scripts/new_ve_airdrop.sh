b25c57626fbf13a995b6233dcb22b70cc99196db
santalov <santalov.stas@gmail.com>
2025-09-18 12:08:52 +0500

    allow split

--- airdrop/scripts/new_ve_airdrop.sh
+++ airdrop/scripts/new_ve_airdrop.sh
@@ -6,7 +6,7 @@ sui client ptb \
   --sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
   --split-coins @0xef1835311f0d615bf11b2aa2068231e724a8809080ad923393adfe867f929cf0 "[388000000000]" \
   --assign new_coins \
-  --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[248,28,113,2,8,12,148,129,22,225,250,34,189,31,91,149,80,87,109,35,60,168,215,32,143,6,241,234,246,81,111,228] 1758096441969 @$CLOCK \
+  --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[248,28,113,2,8,12,148,129,22,225,250,34,189,31,91,149,80,87,109,35,60,168,215,32,143,6,241,234,246,81,111,228] 1758126622868 @$CLOCK \
   --assign airdrop \
   --transfer-objects "[airdrop.1]" @0xc3c7b01f09bfb204f93de85afa0a271e5bfabac31e566f7e997b9a8685f18967 \
   --move-call  sui::transfer::public_share_object "<$PACKAGE::ve_airdrop::VeAirdrop<$SAIL_TOKEN_TYPE>>" airdrop.0
--- governance/scripts/toggle_split.sh
+++ governance/scripts/toggle_split.sh
@@ -1,6 +1,6 @@
 source ./export.sh
 
-export ADDRESS=0x0 # 0x0 allows all addresses
+export ADDRESS=0x2f6af136d3be0f7875143f00310e6587d4a14a54543c6718ddbc51b91418589e # 0x0 allows all addresses
 export ALLOWED=true
 
 sui client ptb --move-call $VE_PACKAGE::voting_escrow::toggle_split "<$FULLSAIL_TOKEN_TYPE>" @$VOTING_ESCROW @$TEAM_CAP @$ADDRESS $ALLOWED
