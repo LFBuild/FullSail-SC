152975a7e683ca589162fcd2fda2c3dea5f811d6
santalov <santalov.stas@gmail.com>
2025-10-08 20:56:01 +0500

    upgrade distribution package

--- build_all.sh
+++ build_all.sh
@@ -5,3 +5,5 @@ cd ./integrate && sui move build && cd ../
 cd ./airdrop && sui move build && cd ../
 cd ./voting_escrow && sui move build && cd ../
 cd ./governance && sui move build && cd ../
+cd ./distribution && sui move build && cd ../
+cd ./legacy/ve && sui move build && cd ../../
