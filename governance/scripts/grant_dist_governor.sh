a572084935736c92d0fa069ceb745f78ee3f791d
santalov <santalov.stas@gmail.com>
2025-10-15 23:23:49 +0500

    setup distribution

--- governance/scripts/grant_dist_governor.sh
+++ governance/scripts/grant_dist_governor.sh
@@ -1,7 +1,6 @@
 source ./export.sh
 
-export WHO=0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3
+export WHO=0xe28ed0b47bc4561cf70b0a2b058c530320f6ed109eebe0e8b59196990751961c
 
 sui client ptb \
---sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
 --move-call $PACKAGE::minter::grant_distribute_governor @$MINTER_PUBLISHER @$WHO
