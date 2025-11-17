8b7835c14324c0b9f83c3e6ff750b2c7f70712f8
Alexey <komalex203@gmail.com>
2025-11-18 01:07:21 +0300

    added scripts

--- governance/scripts/reset_final_voted_weights.sh
+++ governance/scripts/reset_final_voted_weights.sh
@@ -1,7 +1,7 @@
 source ./export.sh
-source ./pools/pool_eth_usdc.sh
+source ./pools/pool_mmt_usdc.sh
 
-export EPOCH_START=1759968000
+export EPOCH_START=1761782400
 export DISTRIBUTE_GOVERNOR_CAP=0xf5f335807046541711fbb00021927baad8798944eceb0cd4986e229def484ab6
 
 sui client ptb \
