6a5580e618d4fc24dc14b511f92f46a170c5d46e
santalov <santalov.stas@gmail.com>
2025-12-24 23:54:16 +0500

    scripts

--- airdrop/scripts/new_ve_airdrop.sh
+++ airdrop/scripts/new_ve_airdrop.sh
@@ -4,9 +4,9 @@ source ./export.sh
 
 sui client ptb \
   --sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
-  --split-coins @0xef1835311f0d615bf11b2aa2068231e724a8809080ad923393adfe867f929cf0 "[2356820343527]" \
+  --split-coins @0xef1835311f0d615bf11b2aa2068231e724a8809080ad923393adfe867f929cf0 "[14180125606714]" \
   --assign new_coins \
-  --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[87,135,190,32,27,63,130,56,243,190,186,180,134,12,7,152,49,14,109,229,174,45,116,43,252,50,145,166,92,73,115,58] 1763555589046 @$CLOCK \
+  --move-call $PACKAGE::ve_airdrop::new "<$SAIL_TOKEN_TYPE>" new_coins.0 vector[209,0,32,94,8,200,62,41,73,43,49,168,150,2,244,247,144,207,228,8,194,247,187,53,178,91,86,156,15,166,113,156] 1766538972627 @$CLOCK \
   --assign airdrop \
   --transfer-objects "[airdrop.1]" @0xc3c7b01f09bfb204f93de85afa0a271e5bfabac31e566f7e997b9a8685f18967 \
   --move-call  sui::transfer::public_share_object "<$PACKAGE::ve_airdrop::VeAirdrop<$SAIL_TOKEN_TYPE>>" airdrop.0
