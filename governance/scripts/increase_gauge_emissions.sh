b33100de2459e01255f0cae5c6434fafeb4b8503
santalov <santalov.stas@gmail.com>
2026-01-17 23:38:16 +0500

    new pool

--- governance/scripts/increase_gauge_emissions.sh
+++ governance/scripts/increase_gauge_emissions.sh
@@ -2,7 +2,7 @@
 
 source ./export.sh
 
-source ./pools/pool_l0wbtc_usdc.sh
+source ./pools/pool_deep_wal.sh
 
 # Save gauge pool variables first
 export GAUGE_POOL=$POOL
@@ -16,7 +16,7 @@ export SAIL_POOL_COIN_A=$COIN_A
 export SAIL_POOL_COIN_B=$COIN_B
 
 # Set emissions increase amount in USD (with 6 decimals, e.g., 1000000 = 1 USD)
-export EMISSIONS_INCREASE_USD=3196173476
+export EMISSIONS_INCREASE_USD=905739409
 
 sui client ptb \
 --move-call $PACKAGE::minter::increase_gauge_emissions "<$GAUGE_COIN_A,$GAUGE_COIN_B,$SAIL_POOL_COIN_A,$SAIL_POOL_COIN_B,$FULLSAIL_TOKEN_TYPE>" @$MINTER @$VOTER @$DISTRIBUTION_CONFIG @$MINTER_ADMIN_CAP @$GAUGE_ID @$GAUGE_POOL $EMISSIONS_INCREASE_USD @$PRICE_MONITOR @$SAIL_POOL @$AGGREGATOR @$CLOCK 
