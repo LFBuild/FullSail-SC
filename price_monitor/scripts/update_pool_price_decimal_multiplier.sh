f7abb759acf611a740cbfaf3afa6f79750c7ef11
santalov <santalov.stas@gmail.com>
2026-02-01 19:21:57 +0500

    upgrade price monitor

--- price_monitor/scripts/update_pool_price_decimal_multiplier.sh
+++ price_monitor/scripts/update_pool_price_decimal_multiplier.sh
@@ -4,5 +4,5 @@ source ./export.sh
 POOL_ID=0x2acf170284a279b79cde66d747697cbd4560d0db6d6fd04c078a01efd1973efa
 
 sui client ptb \
---move-call $PACKAGE::price_monitor::update_pool_price_decimal_multiplier_v2 @$MONITOR @$POOL_ID 6 9 --dry-run
+--move-call $PACKAGE::price_monitor::update_pool_price_decimal_multiplier_v2 @$MONITOR @$POOL_ID 6 9 
 
