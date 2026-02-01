4b1b7435db372c424e35ed9d8f1b779ed1898f5b
santalov <santalov.stas@gmail.com>
2026-02-01 17:37:18 +0500

    upgrade dev contracts

--- price_monitor/scripts/update_pool_price_decimal_multiplier.sh
+++ price_monitor/scripts/update_pool_price_decimal_multiplier.sh
@@ -1,7 +1,8 @@
 source ./export.sh
 
 # SAIL/USDC
-POOL_ID=0x038eca6cc3ba17b84829ea28abac7238238364e0787ad714ac35c1140561a6b9
+POOL_ID=0x2acf170284a279b79cde66d747697cbd4560d0db6d6fd04c078a01efd1973efa
 
 sui client ptb \
---move-call $PACKAGE::price_monitor::update_pool_price_decimal_multiplier_v2 @$MONITOR @$POOL_ID 6 6
+--move-call $PACKAGE::price_monitor::update_pool_price_decimal_multiplier_v2 @$MONITOR @$POOL_ID 6 9 --dry-run
+
