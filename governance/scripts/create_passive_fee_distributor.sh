5a0c1261381222c41eadd82799f3f3966b88311d
santalov <santalov.stas@gmail.com>
2026-02-19 02:17:47 +0500

    create passive fee distributor for dev

---
+++ governance/scripts/create_passive_fee_distributor.sh
@@ -0,0 +1,9 @@
+source ./export.sh
+
+# Example fee token for passive-fee distribution (update as needed).
+export PASSIVE_FEE_COIN=0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC
+
+sui client ptb \
+--move-call $PACKAGE::minter::create_and_start_passive_fee_distributor "<$FULLSAIL_TOKEN_TYPE,$PASSIVE_FEE_COIN>" @$MINTER @$MINTER_ADMIN_CAP @$VOTING_ESCROW @$DISTRIBUTION_CONFIG @$CLOCK \
+--assign passive_fee_distributor \
+--move-call sui::transfer::public_share_object "<0x1b7c61939e96522ff8b36cd5ad1c971684e885422c30d65fbe2ffa08c318aee1::passive_fee_distributor::PassiveFeeDistributor<$PASSIVE_FEE_COIN>>" passive_fee_distributor
--- governance/scripts/set_package_version.sh
+++ governance/scripts/set_package_version.sh
@@ -2,4 +2,4 @@ source ./export.sh
 # --sender @0xfed1c619fc8dd98367a0422ca9ef53c9825e2893d78dda822106d12687888fb3 --gas-coin @0xe6600100e23ccefd343837558242768b59dacca73b28132482e1b0ec6370c81b --gas-budget 100000000 --serialize-unsigned-transaction \
 
 sui client ptb \
---move-call $PACKAGE::distribution_config::set_package_version @$DISTRIBUTION_CONFIG @$DISTRIBUTION_CONFIG_PUBLISHER 5
+--move-call $PACKAGE::distribution_config::set_package_version @$DISTRIBUTION_CONFIG @$DISTRIBUTION_CONFIG_PUBLISHER 6
