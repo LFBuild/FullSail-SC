248700d0ed78f91978d86b52e1472e8a46142a97
santalov <santalov.stas@gmail.com>
2026-05-02 00:42:16 +0500

    new drop

--- airdrop/scripts/new_airdrop.sh
+++ airdrop/scripts/new_airdrop.sh
@@ -2,11 +2,11 @@ source ./export.sh
 
 export COIN_TYPE=0x1018b0843a724fd966b37f018bbc489918b6594144451cf4b481d392c9a0a463::token_a::TOKEN_A
 export COIN_ID=0x5f74257bcefb22a1e8ff05dac62ae5836b49cfe57c59f678c8052b192abfee46
-export START_TIME_MS=1777617408094
+export START_TIME_MS=1777639913766
 
 sui client ptb \
-  --split-coins @$COIN_ID "[30000000000]" \
+  --split-coins @$COIN_ID "[20000000000]" \
   --assign new_coins \
-  --move-call $PACKAGE::airdrop::new "<$COIN_TYPE>" new_coins.0 vector[188,193,100,23,96,82,189,31,212,91,5,184,238,241,114,218,178,136,149,103,141,56,109,124,37,231,206,156,155,83,45,149] $START_TIME_MS @$CLOCK \
+  --move-call $PACKAGE::airdrop::new "<$COIN_TYPE>" new_coins.0 vector[176,166,158,18,24,232,91,106,26,42,245,36,243,244,68,166,84,196,12,156,241,201,238,242,170,68,240,93,218,71,36,54] $START_TIME_MS @$CLOCK \
   --assign airdrop \
   --move-call  sui::transfer::public_share_object "<$PACKAGE::airdrop::Airdrop<$COIN_TYPE>>" airdrop
